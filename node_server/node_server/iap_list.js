af9520b5a91c1402095c62c913912fd0574eab96
donghui.sun <donghui.sun@ihandysoft,com>
2019-03-19 16:56:26 +0800

    try anagin

--- node_server/node_server/iap_list.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "metric": [
-        ["2017-12-19", 50, 1],
-        ["2017-12-18", 60, 33, 2]
-    ],
-    "summary": [11, 12, 13],
-     "pay_ratio": 0.11,
-     "pay_ratio_first_day": 0.12
-};
--- node_server/node_server/user_list.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    users: [
-        "wenjie.yang",
-        "yao.deng",
-        "donghui.sun"
-    ]
-}
